3336d56b5b339fa2b696556cfd363b64a7d06c0a
”YanaiMaho” <maholeonpalann@i.softbank.jp>
2020-08-19 10:37:32 +0000

    PHP/Laravel 課題

--- PlofileController.php
+++ PlofileController.php
@@ -5,15 +5,31 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class PlofileController extends Controller
+use App\Profiles;
+
+class ProfileController extends Controller
 {
-    public function add()
+     public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+       // Varidationを行う
+      $this->validate($request, Profiles::$rules);
+
+      $profiles = new Profiles;
+      $form = $request->all();
+
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+
+      // データベースに保存する
+      $profiles->fill($form);
+      $profiles->save();
+
         return redirect('admin/profile/create');
     }
 
